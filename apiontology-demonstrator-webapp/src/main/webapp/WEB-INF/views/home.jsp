a8c2c031bdfc36c00fe326a8391527eb74a1feaa
m.schmidt <manuel.schmidt@unister.de>
2015-07-31 14:36:37 +0200

    Implemented frontend

--- apiontology-demonstrator-webapp/src/main/webapp/WEB-INF/views/home.jsp
+++ apiontology-demonstrator-webapp/src/main/webapp/WEB-INF/views/home.jsp
@@ -9,16 +9,56 @@
 <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
 <script type="text/javascript" src="js/angular-1.4.1.min.js"></script>
 <script type="text/javascript" src="js/highlight.pack.js"></script>
+<script type="text/javascript" src="js/aod.js"></script>
 </head>
 
-<body data-ng-app>
-<pre><code class="owlms">${ontology}</code></pre>
+<body class="table"
+	data-ng-app="aodApp"
+	data-ng-controller="MainController"
+	data-ng-init="init()">
+	<div class="left content cell">
+		<div>
+			<div class="label">Constraints</div>
+			<div class="table box constraints">
+				<div class="row constraint"
+					data-ng-repeat="constraint in configurations.constraints">
+					<div class="cell">{{ constraint.webService }}</div>
+					<div class="cell parameters">
+						<div class="option parameter"
+							data-ng-repeat="parameter in constraint.parameters"
+							data-ng-class="{ 'selected' : constraint.selected[$index] }"
+							data-ng-click="constraint.selected[$index] = !constraint.selected[$index]">{{ parameter }}</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div>
+			<div class="label">Parameter Equivalences</div>
+			<div class="table box equivalences">
+				<div class="equivalence row"
+					data-ng-repeat="equivalence in configurations.equivalences">
+					<div class="cell parameter">{{ equivalence.parameter }}</div>
+					<div class="cell">&equiv;</div>
+					<div class="cell eq-parameters">
+						<div class="option eq-parameter"
+							data-ng-repeat="eqParameter in equivalence.eqParameters"
+							data-ng-class="{ 'selected' : equivalence.selected[$index] }"
+							data-ng-click="equivalence.selected[$index] = !equivalence.selected[$index]">{{ eqParameter }}</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<button
+			data-ng-click="submit()">Submit</button>
+	</div>
+
+	<div class="right content cell">
+		<div class="label">Ontotlogy</div>
+		<div>
+			<div class="datamodel">{{ configurations.datamodel }}</div>
+		</div>
+	</div>
 </body>
-<script type="text/javascript">
-$(document).ready(function() {
-	  $('pre code').each(function(i, block) {
-	    hljs.highlightBlock(block);
-	  });
-	});
-</script>
 </html>
---
+++ apiontology-demonstrator-webapp/src/main/webapp/js/aod.js
@@ -0,0 +1,86 @@
+var aodApp = angular.module('aodApp', []);
+
+aodApp.controller('MainController', [ '$scope', '$element', '$http', function(s, $element, $http) {
+    s.init = function() {
+        // TODO remove mock
+        s.configurations = {
+            constraints : [ {
+                webService : 'web-service-1',
+                parameters : [ 'parameter-1', 'parameter-2', 'parameter-3' ]
+            }, {
+                webService : 'web-service-2',
+                parameters : [ 'parameter-1', 'parameter-2', 'parameter-3' ]
+            }, {
+                webService : 'web-service-3',
+                parameters : [ 'parameter-1', 'parameter-2', 'parameter-3' ]
+            } ],
+            equivalences : [ {
+                parameter : 'parameter-1',
+                eqParameters : [ 'eq-parameter-1', 'eq-parameter-2', 'eq-parameter-3' ]
+            }, {
+                parameter : 'parameter-2',
+                eqParameters : [ 'eq-parameter-1', 'eq-parameter-2', 'eq-parameter-3' ]
+            }, {
+                parameter : 'parameter-3',
+                eqParameters : [ 'eq-parameter-1', 'eq-parameter-2', 'eq-parameter-3' ]
+            } ],
+            datamodel : 'datamodel'
+        };
+
+        $http.post('/configurations.json').success(function(configurations) {
+            s.configurations = configurations;
+        });
+    };
+
+    var selectedConstraints = function() {
+        var result = [];
+        s.configurations.constraints.forEach(function(constraint) {
+            var selected = [];
+            for (var index = 0; index < constraint.parameters.length; index++) {
+                if (constraint.hasOwnProperty('selected') && constraint.selected[index]) {
+                    selected.push(constraint.parameters[index]);
+                }
+            }
+            if (selected.length > 0) {
+                result.push({
+                    webService : constraint.webService,
+                    parameters : selected
+                });
+            }
+        });
+        return result;
+    }
+
+    var selectedEquivalences = function() {
+        var result = [];
+        s.configurations.equivalences.forEach(function(equivalence) {
+            var selected = [];
+            for (var index = 0; index < equivalence.eqParameters.length; index++) {
+                if (equivalence.hasOwnProperty('selected') && equivalence.selected[index]) {
+                    selected.push(equivalence.eqParameters[index]);
+                }
+            }
+            if (selected.length > 0) {
+                result.push({
+                    parameter : equivalence.parameter,
+                    eqParameters : selected
+                });
+            }
+        });
+        return result;
+    }
+
+    s.submit = function() {
+        var configurations = {
+            constraints : selectedConstraints(),
+            equivalences : selectedEquivalences(),
+            datamodel : null
+        };
+
+        console.log(configurations)
+
+        $http.post('/configurations.json', configurations).success(function(configurations) {
+            s.configurations = configurations;
+        });
+    };
+} ]);
