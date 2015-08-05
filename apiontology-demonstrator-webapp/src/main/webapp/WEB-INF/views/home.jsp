6bc4d04e6359666ace954140eafd259ca439b563
m.schmidt <manuel.schmidt@unister.de>
2015-08-05 13:03:32 +0200

    Implemented showing experiment input and new way of data exchange

--- apiontology-demonstrator-webapp/src/main/webapp/WEB-INF/views/home.jsp
+++ apiontology-demonstrator-webapp/src/main/webapp/WEB-INF/views/home.jsp
@@ -12,56 +12,103 @@
 <script type="text/javascript" src="js/aod.js"></script>
 </head>
 
-<body class="table"
+<body
 	data-ng-app="aodApp"
 	data-ng-controller="MainController"
 	data-ng-init="init()">
-	<div class="left content cell">
+	<div class="left content">
 		<div>
-			<div class="label">Constraints</div>
-			<div class="table box constraints">
-				<div class="row constraint"
-					data-ng-repeat="constraint in configurations.constraints">
-					<div class="cell">{{ constraint.webService }}</div>
-					<div class="cell parameters">
-						<div class="option parameter"
-							data-ng-repeat="parameter in constraint.parameters"
-							data-ng-class="{ 'selected' : constraint.selected[$index] }"
-							data-ng-click="constraint.selected[$index] = !constraint.selected[$index]">{{ parameter }}</div>
-					</div>
-				</div>
+			<div class="label">Ontology URI</div>
+			<table class="box uri">
+				<tr>
+					<td class="input">
+						<input
+							type="text"
+							data-ng-model="uri"/>
+					</td>
+					<td>
+						<button
+							data-ng-click="addUri()">Add</button>
+					</td>
+				</tr>
+			</table>
+		</div>
+
+		<div data-ng-show="hasConstraintsAndEquivalences()">
+			<div>
+				<div class="label">Constraints</div>
+				<table class="box constraints">
+					<tr class="constraint"
+						data-ng-repeat="constraint in configurations.constraints">
+						<td>{{ constraint.webService }}</td>
+						<td class="parameters">
+							<div class="option parameter"
+								data-ng-repeat="parameter in constraint.parameters"
+								data-ng-class="{ 'selected' : constraint.selected[$index] }"
+								data-ng-click="constraint.selected[$index] = !constraint.selected[$index]">{{ parameter }}</div>
+						</td>
+					</tr>
+				</table>
+			</div>
+
+			<div>
+				<div class="label">Parameter Equivalences</div>
+				<table class="box equivalences">
+					<tr class="equivalence"
+						data-ng-repeat="equivalence in configurations.equivalences">
+						<td class="parameter">{{ equivalence.parameter }}</td>
+						<td>&equiv;</td>
+						<td class="eq-parameters">
+							<div class="option eq-parameter"
+								data-ng-repeat="eqParameter in equivalence.eqParameters"
+								data-ng-class="{ 'selected' : equivalence.selected[$index] }"
+								data-ng-click="equivalence.selected[$index] = !equivalence.selected[$index]">{{ eqParameter }}</div>
+						</td>
+					</tr>
+				</table>
+			</div>
+
+			<div class="button-container">
+				<button
+					data-ng-click="submitConfig()">Submit parameter equivalence configuration</button>
 			</div>
 		</div>
 
-		<div>
-			<div class="label">Parameter Equivalences</div>
-			<div class="table box equivalences">
-				<div class="equivalence row"
-					data-ng-repeat="equivalence in configurations.equivalences">
-					<div class="cell parameter">{{ equivalence.parameter }}</div>
-					<div class="cell">&equiv;</div>
-					<div class="cell eq-parameters">
-						<div class="option eq-parameter"
-							data-ng-repeat="eqParameter in equivalence.eqParameters"
-							data-ng-class="{ 'selected' : equivalence.selected[$index] }"
-							data-ng-click="equivalence.selected[$index] = !equivalence.selected[$index]">{{ eqParameter }}</div>
-					</div>
-				</div>
+		<div
+			data-ng-show="hasExperimentInput()">
+			<div>
+				<div class="label">Experiment Input</div>
+				<table class="box">
+					<tr class="experiment-input"
+						data-ng-repeat="(parameter, value) in experimentInput">
+						<td class="experiment-input-parameter">{{ parameter }}</td>
+						<td class="experiment-input-value">
+							<input
+								type="text"
+								data-ng-model="experimentInput[parameter]"/>
+						</td>
+					</tr>
+				</table>
+			</div>
+
+			<div class="button-container">
+				<button
+					data-ng-click="submitInput()">Submit experiment input</button>
 			</div>
 		</div>
-
-		<button
-			data-ng-click="submit()">Submit</button>
 	</div>
 
-	<div class="right content cell">
-		<div class="label">Ontology</div>
-		<div class="table box datamodel"><%--
-		--%><div class="row"><%--
-			--%><div class="cell"
-				data-ng-bind-html="datamodel()"></div><%--
-			--%></div><%--
-		--%></div>
+	<div class="right content">
+		<div
+			data-ng-show="hasDatamodel()">
+			<div class="label">Ontology</div>
+			<table class="box datamodel">
+				<tr>
+					<td
+						data-ng-bind-html="datamodel()"></td>
+				</tr>
+			</table>
+		</div>
 	</div>
 </body>
 </html>
--- apiontology-demonstrator-webapp/src/main/webapp/js/aod.js
+++ apiontology-demonstrator-webapp/src/main/webapp/js/aod.js
@@ -1,63 +1,98 @@
 var aodApp = angular.module('aodApp', []);
 
 aodApp.controller('MainController', [ '$scope', '$element', '$http', '$sce', function(s, $element, $http, $sce) {
-    s.init = function() {
-        $http.get('configurations.json').success(function(configurations) {
-            s.configurations = configurations;
-        });
+    var hasConfigProperty = function(property) {
+        return s.configurations && s.configurations[property] && s.configurations[property] instanceof Array && s.configurations[property].length > 0
     };
 
     var selectedConstraints = function() {
         var result = [];
-        s.configurations.constraints.forEach(function(constraint) {
-            var selected = [];
-            for (var index = 0; index < constraint.parameters.length; index++) {
-                if (constraint.hasOwnProperty('selected') && constraint.selected[index]) {
-                    selected.push(constraint.parameters[index]);
+        if (hasConfigProperty('constraints')) {
+            s.configurations.constraints.forEach(function(constraint) {
+                var selected = [];
+                for (var index = 0; index < constraint.parameters.length; index++) {
+                    if (constraint.hasOwnProperty('selected') && constraint.selected[index]) {
+                        selected.push(constraint.parameters[index]);
+                    }
                 }
-            }
-            if (selected.length > 0) {
-                result.push({
-                    webService : constraint.webService,
-                    parameters : selected
-                });
-            }
-        });
+                if (selected.length > 0) {
+                    result.push({
+                        webService : constraint.webService,
+                        parameters : selected
+                    });
+                }
+            });
+        }
         return result;
     }
 
     var selectedEquivalences = function() {
         var result = [];
-        s.configurations.equivalences.forEach(function(equivalence) {
-            var selected = [];
-            for (var index = 0; index < equivalence.eqParameters.length; index++) {
-                if (equivalence.hasOwnProperty('selected') && equivalence.selected[index]) {
-                    selected.push(equivalence.eqParameters[index]);
+        if (hasConfigProperty('equivalences')) {
+            s.configurations.equivalences.forEach(function(equivalence) {
+                var selected = [];
+                for (var index = 0; index < equivalence.eqParameters.length; index++) {
+                    if (equivalence.hasOwnProperty('selected') && equivalence.selected[index]) {
+                        selected.push(equivalence.eqParameters[index]);
+                    }
                 }
-            }
-            if (selected.length > 0) {
-                result.push({
-                    parameter : equivalence.parameter,
-                    eqParameters : selected
-                });
+                if (selected.length > 0) {
+                    result.push({
+                        parameter : equivalence.parameter,
+                        eqParameters : selected
+                    });
+                }
+            });
+        }
+        return result;
+    };
+
+    var exchange = function(action, request) {
+        $http.post('exchange.json', request).success(function(response) {
+            s.configurations = respone.configurations;
+            s.experimentInput = response.experimentInput;
+        });
+    };
+
+    s.init = function() {
+        exchange('init', {});
+    };
+
+    s.addUri = function() {
+        exchange('add uri', {
+            uri : s.uri
+        });
+        s.uri = '';
+    };
+
+    s.submitConfig = function() {
+        exchange('submit config', {
+            configurations : {
+                constraints : selectedConstraints(),
+                equivalences : selectedEquivalences()
             }
         });
-        return result;
-    }
+    };
 
-    s.submit = function() {
-        var configurations = {
-            constraints : selectedConstraints(),
-            equivalences : selectedEquivalences(),
-            datamodel : null
-        };
-
-        $http.post('configurations.json', configurations).success(function(configurations) {
-            s.configurations = configurations;
+    s.submitInput = function() {
+        exchange('submit input', {
+            experimentInput : s.experimentInput
         });
     };
 
     s.datamodel = function() {
         return $sce.trustAsHtml(s.configurations && s.configurations.datamodel ? s.configurations.datamodel : '');
+    };
+
+    s.hasConstraintsAndEquivalences = function(property) {
+        return hasConfigProperty('constraints') && hasConfigProperty('equivalences');
+    };
+
+    s.hasExperimentInput = function() {
+        return s.experimentInput && Object.keys(s.experimentInput).length > 0;
+    };
+
+    s.hasDatamodel = function() {
+        return s.configurations && s.configurations.datamodel;
     };
 } ]);
